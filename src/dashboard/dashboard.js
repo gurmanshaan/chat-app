69db0960eb45bd9137c12e7a447a66225fee1e8a
gurmanshaan <gurmanshaan@gmail.com>
2021-05-06 19:16:44 +0530

    completed displaying the chat list

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,10 +1,65 @@
 import React from 'react';
-
+import ChatListComponent from '../chatlist/chatlist';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 class DashboardComponent extends React.Component {
 
-    render() {
-        return (<div>dashboard</div>)
+    constructor() {
+        super();
+        this.state = {
+            selectedChat: null,
+            newChatFormVisible: false,
+            email: null,
+            chats: []
+        };
     }
+
+    render() {
+        return (
+            <div>
+                <ChatListComponent
+                    history={this.props.hostory}
+                    newChatBtnFn={this.newChatBtnClicked}
+                    selectChatFn={this.selectChat}
+                    chats={this.state.chats}
+                    userEmail={this.state.email}
+                    selectedChatIndex={this.state.selectedChat}
+                ></ChatListComponent>
+            </div>
+        )
+    }
+
+    selectChat = (chatIndex) => {
+        console.log('select chat', chatIndex)
+    }
+
+    newChatBtnClicked = () => this.setState({
+        newChatFormVisible: true, selectedChat: null
+    })
+
+    componentDidMount = () => {
+        firebase
+            .auth()
+            .onAuthStateChanged(async _usr => {
+                if (!_usr)
+                    this.props.history.push('/login');
+                else {
+                    await firebase
+                        .firestore()
+                        .collection('chats')
+                        .where('users', 'array-contains', _usr.email)
+                        .onSnapshot(async res => {
+                            const chats = res.docs.map(_doc => _doc.data())
+                            await this.setState({
+                                email: _usr.email,
+                                chats: chats
+                            });
+                            console.log(this.state);
+                        })
+                }
+            })
+    }
+
 }
 
 export default DashboardComponent;
